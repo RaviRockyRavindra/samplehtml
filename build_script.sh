438c6d674e9f38043a10f9aa9fd6a5ea37b891f9
K.Ravindra sai <37626998+RaviRockyRavindra@users.noreply.github.com>
2019-12-24 12:09:58 +0530

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -2,7 +2,7 @@ sudo apt update -y
 sudo apt install docker.io -y
 sudo service docker start
 sudo docker build . -t  httpdimage 
-sudo docker run -d -v /tmp/tempdir/githubjob:/usr/local/apache2/htdocs -p81:80 httpdimage
+sudo docker run -d -v /tmp/tempdirr/githubjob:/usr/local/apache2/htdocs -p81:80 httpdimage
 
 #sudo docker run doesnot contains run -it because we are executing in detached mode so dont forgot to attach -d
 

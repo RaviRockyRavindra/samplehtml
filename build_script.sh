dc2a9fdbc5b1d9125efa0862648fee65a9d024d4
K.Ravindra sai <37626998+RaviRockyRavindra@users.noreply.github.com>
2019-12-24 11:13:53 +0530

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -2,4 +2,4 @@ sudo apt update -y
 sudo apt install docker.io -y
 sudo service docker start
 sudo docker build . -t  httpdimage 
-sudo docker run -it -v /tmp/tempdir/:/usr/local/apache2/htdocs httpdimage
+sudo docker run -it -v /tmp/tempdir/githubjob:/usr/local/apache2/htdocs httpdimage
